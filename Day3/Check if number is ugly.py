f77eb7258396c881fa3a84d92aec628406f12b9c
SakthiKs24 <kssakthi2002@gmail.com>
2025-07-14 18:33:10 +0530

    Ugly Number
    Ugly Number

---
+++ Day3/Check if number is ugly.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isUgly(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+
+        for p in [2, 3, 5]:
+            while n % p == 0:
+                n //= p
+
+        return n == 1
+
+if __name__ == "__main__":
+    n = int(input("Enter an integer: "))
+    solution = Solution()
+    result = solution.isUgly(n)
+    print("Is ugly number:", result)
