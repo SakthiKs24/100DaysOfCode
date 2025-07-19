4d2496651f5a0228ffa08275a9d77f65221068b6
SakthiKs24 <kssakthi2002@gmail.com>
2025-07-19 18:35:11 +0530

    Day8 - GroupAnagrams
    Day8 - GroupAnagrams

---
+++ Day8/GroupAnagrams.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagram_map = defaultdict(list)
+
+        for word in strs:
+            sorted_word = tuple(sorted(word))
+            anagram_map[sorted_word].append(word)
+
+        return list(anagram_map.values())
+
+user_input = input("Enter words separated by space: ")
+strs = user_input.strip().split()
+sol = Solution()
+result = sol.groupAnagrams(strs)
+print("Grouped Anagrams:", result)
